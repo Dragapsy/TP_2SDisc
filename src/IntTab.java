adc8a797f7ab2cd92379ba8e92541d913dfa28cf
dragapsy <elghaoutiayman20@gmail.com>
2022-10-04 01:11:39 +0200

    maj

--- src/IntTab.java
+++ src/IntTab.java
@@ -1,49 +1,129 @@
+
+//                          EL GHAOUTI AYMANE GROUPE 3 TP2 rendu
+
 public class IntTab {
     private final int emptyElement;
-    private static final int MAX_SIZE = 10000;
+    private static final int MAX_SIZE =  10000;
     int[] tab = new int[MAX_SIZE];
     int size;
-// EXO 1 :
-    // Reponse dans le main en commentaire !
+    // EXO 1 :
+         // Reponse dans le Main en commentaire !
 
-// EXO 2 :
-    public IntTab( int size, int emptyElement) {
-        this.emptyElement = emptyElement;
-        this.size = 0;
-    }
-
-    public IntTab(int[] tab2, int emptyElement, int s) {
-        this.tab = tab2;
-        this.emptyElement = emptyElement;
-        this.size = s;
+    // EXO 2 :
+    public IntTab(){
+        this.emptyElement=0;
+        this.size=0;
     }
 
-    public IntTab(int e, int[] tab3, int s) {
-        this.emptyElement = e;
-        this.tab = tab3;
-        this.size = s;
-    }
-// EXO 3 :
-    public int getSize() {
-        return tab.length;
-    }
-// EXO 4 :
-    public int get(int i) {
-        if (i> (tab.length - 1)){
-            return emptyElement;
+    public IntTab(int s){
+        this.size=s;
+        this.emptyElement=0;
+        for(int i=0; i<s; i++){
+            tab[i]=emptyElement;
         }
-        return tab[i];
     }
-    public boolean set(int data, int i) {
-        if (i > (tab.length - 1)) {
+
+    public IntTab(int size, int e){
+        this.size=size;
+        this.emptyElement=e;
+        for(int i=0; i<size; i++){
+            tab[i]=emptyElement;
+        }
+    }
+    // EXO 3 :
+    public int getSize(){
+        return this.size;
+    }
+    // EXO 4 :
+    public int get(int i){
+        if(i>=0 && i<size){
+            return tab[i];
+        }else{
+            return this.emptyElement;
+        }
+
+    }
+
+    public boolean set(int data, int i){
+        if(i>=0 && i<size){
+            tab[i]=data;
+            return true;
+        }else{
             return false;
         }
-        tab[i] = data;
-        return true;
     }
-// EXO 5 :
+    // EXO 5 :
     public boolean addLast(int e){
-        if ()
-        return false;
+        int lastPosition=(this.size);
+        if(lastPosition < MAX_SIZE){
+            tab[lastPosition]=e;
+            this.size++;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    // EXO 6 :
+    public boolean resize(int s){
+        if(s > size && s <= MAX_SIZE){
+            for(int i=size; i<s; i++){
+                tab[i]=this.emptyElement;
+            }
+            this.size=s;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    // EXO 7 :
+    public boolean insert(int data, int i){
+        if(size+1 <=MAX_SIZE && i>=0 && i<MAX_SIZE){
+            int temp1=tab[i];
+            int temp2=tab[i+1];
+            tab[i]=data;
+            for(int j=i+1; j<(size+1); j++){
+                tab[j]=temp1;
+                temp1=temp2;
+                if(j+1 < MAX_SIZE){
+                    temp2=tab[j+1];
+                }
+            }
+            this.size++;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // EXO 8 :
+    public boolean remove(int i){
+        if(i>=0 && i<size){
+            int temp;
+            for(int j=i; j<(size-1); j++){
+                temp=tab[i];
+                tab[j]=tab[j+1];
+                tab[j+1]=temp;
+            }
+            tab[size-1]=0;
+            this.size--;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // EXO 9 :
+    @Override
+    public String toString() {
+        String ch="Tab=[";
+        for(int i=0; i<size; i++){
+            if(i<size-1){
+                ch+=String.valueOf(tab[i]+",");
+            }else{
+                ch+=String.valueOf(tab[i]);
+            }
+        }
+        ch+="]";
+        return ch;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,49 @@
 public class Main {
     static int[] arrayOfInts = {0,1,2,3,4};
+
     static void change (int[] t){
         t[0] = 5;
     }
-    public static void main(String[] args) {
 
+    public static void main(String args[]){
         change(arrayOfInts);
-        System.out.println(arrayOfInts[0]);
-    }
-    /* comme la fonction prend en parametre un tableau d'entier,
-     et qu'on l'a appelé avec un tableau d'entier elle s'est permise
-     de prendre la 1er case du tableau et changer sa valeur par [5],
-      ce qui nous donne un tableau final de { 5 ; 1 ; 2 ; 3 ; 4 }*/
+       // System.out.println(arrayOfInts[0]);
+        //EXO 1:
+        /*  la methode "change ()" agit sur un tableau renseigné dans sa signature dont ses elements sont
+//      des entiers .
+//      elle permets de remplacer la valeur qui est positinnée à l'index 0 du tableau par 5.*/
 
+        //Création d'un tableau vide
+        IntTab liste=new IntTab();
+        //Ajout d'élément à la fin du tableau
+        liste.addLast(1);
+        liste.addLast(2);
+        liste.addLast(3);
+        liste.addLast(4);
+        liste.addLast(5);
+
+        //affichage du tableau avant toute opération
+        System.out.println(liste.getSize());
+        System.out.println(liste.toString());
+
+        //récupération et modification d'un élément dans le tableau
+        System.out.println(liste.get(4)); // 5
+        liste.set(7, 3); /*tab=[1,2,3,7,5]
+                           => (resultat attendu en appliquant la methode ".set()" avant toute opération)*/
+
+        //suppremer un élément du tableau
+        liste.remove(2);  /*Tab=[1,2,4,5]
+                           => (resultat attendu en appliquant la methode ".remove()" avant toute opération)*/
+
+        //Insertion à la position i
+        liste.insert(8, 2); /*Tab=[1,2,8,3,4,5]
+                           => (resultat attendu en appliquant la methode ".insert()" avant toute opération)*/
+
+        //Redimenssionner le tableau
+        liste.resize(11); /*Tab=[1,2,3,4,5,0,0,0,0,0,0]
+                           => (resultat attendu en appliquant la methode ".resize()" avant toute opération)*/
+
+        System.out.println(liste.getSize());
+        System.out.println(liste.toString());
+    }
 }
